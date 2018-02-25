c4c407fb442567d018d482ff25f5e10f38cf6bdd
Irvin Lim <irvinlimweiquan@gmail.com>
2018-02-26 02:19:39 +0800

    Update run script to not remove all Docker containers from host

--- run.sh
+++ run.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 
 if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
+then 
+  echo "Please run as root"
   exit
 fi
 
-TEAMID=`md5sum README.md | cut -d' ' -f 1`
-docker kill $(docker ps -q)
-docker rm $(docker ps -a -q)
-docker build . -t $TEAMID
+# Cross-platform MD5
+if [ "$(uname)" == "Darwin" ]; then 
+  # For macOS
+  MD5=`md5 -q README.md`
+else
+  # For Linux
+  MD5=`md5sum README.md`
+fi
+
+TEAMID=`echo $MD5 | cut -d' ' -f 1`
+
+# Stop existing container(s)
+CONTAINERS=`docker ps -a -q --filter ancestor=$TEAMID --filter status=running`
+IFS=$'\n'
+for container in $CONTAINERS
+do
+  echo "Stopping and removing existing container $container..."
+  docker kill $container &> /dev/null
+  docker rm $container &> /dev/null
+done
+
+# Build and start the container.
+docker build -t $TEAMID .
 docker run -p 80:80 -p 8080:8080 -t $TEAMID
