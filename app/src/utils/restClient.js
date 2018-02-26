d0bec62dcbb83f774b020a0c1373c066059c6b4d
Irvin Lim <irvinlimweiquan@gmail.com>
2018-02-26 22:37:43 +0800

    Add getStatus method to the REST client and fetch heartbeat

--- app/src/utils/restClient.js
+++ app/src/utils/restClient.js
@@ -1,20 +1,58 @@
 import axios from 'axios';
 
+/**
+ * Performs a HTTP GET request.
+ *
+ * @param {string} endpoint Endpoint to fetch.
+ * @returns {object|Array} JSON data.
+ */
 export const get = async endpoint => {
+  const url = makeUrl(endpoint);
   return await axios
-    .get(`http://localhost/api${endpoint}`)
-    .then(response => {
-      const data = response.data;
+    .get(url)
+    .then(makeResHandler(url))
+    .catch(makeErrHandler(url));
+};
 
-      // Handle REST errors.
-      if (!data || !data.status) {
-        throw new Error(data.error);
-      }
+/**
+ * Checks if an endpoint returns a valid response, as signaled by the
+ * "status" field in the response JSON body.
+ *
+ * @param {string} endpoint Endpoint to get the status for.
+ * @returns {boolean} Returns true if the endpoint returns true.
+ */
+export const getStatus = async endpoint => {
+  try {
+    const url = makeUrl(endpoint);
+    await axios.get(url).then(makeGetStatusHandler(url));
+  } catch (err) {
+    return false;
+  }
 
-      return data.result;
-    })
-    .catch(e => {
-      // Handle HTTP client errors.
-      throw new Error(e.message);
-    });
+  return true;
+};
+
+const makeUrl = endpoint => {
+  return `http://localhost/api${endpoint}`;
+};
+
+const makeResHandler = url => response => {
+  const data = response.data;
+
+  // Handle REST errors.
+  if (!data || !data.status) {
+    const handler = makeErrHandler(url);
+    throw new Error(data.error);
+  }
+
+  return data.result;
+};
+
+const makeGetStatusHandler = url => response => {
+  const data = response.data;
+  return data && data.status;
+};
+
+const makeErrHandler = url => err => {
+  throw new Error(`Error while fetching ${url}: ${err.message}`);
 };
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -4,6 +4,17 @@
       <h3 class="title is-h3">Secret Diary</h3>
       <h5 class="subtitle is-h5">CS5331 Assignment 1</h5>
       <hr>
+      <div class="content">
+        <h4>REST API Status</h4>
+        <div class="tags has-addons">
+          <div class="tag is-dark">Status</div>
+          <div class="tag is-success" v-if="status">Up</div>
+          <div class="tag is-danger" v-else>Down</div>
+        </div>
+      </div>
+
+      <hr>
+
       <article class="message is-danger" v-if="this.errors.length">
         <div class="message-header">Error</div>
         <div class="message-body content">
@@ -21,17 +32,24 @@
 </template>
 
 <script>
-import { get } from '../utils/restClient';
+import { get, getStatus } from '../utils/restClient';
 
 export default {
   data() {
     return {
+      status: false,
       members: [],
       errors: [],
     };
   },
 
   async created() {
+    try {
+      this.status = await getStatus('/meta/heartbeat');
+    } catch (e) {
+      this.status = false;
+    }
+
     try {
       this.members = await get('/meta/members');
     } catch (e) {
