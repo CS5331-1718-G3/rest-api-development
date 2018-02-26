b2d3380493d4e0896cf6fbbd8de8d664c41c17d0
Irvin Lim <irvinlimweiquan@gmail.com>
2018-02-26 18:14:52 +0800

    Remove unneeded Express boilerplate files and create index route

--- api/app.js
+++ api/app.js
@@ -1,46 +1,38 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const app = express();
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// Set up middlewares.
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// Attach routes.
+app.use('/', require('./routes/index'));
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler.
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // Set locals, only providing error in development.
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Return error status, default to 500 server error.
   res.status(err.status || 500);
-  res.render('error');
+
+  // Show any errors.
+  res.json(res.locals);
 });
 
 module.exports = app;
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// GET / - Retrieve the endpoint list
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // Get all implemented routes from the Express router.
+  const routes = router.stack
+    .map(r => r.route && r.route.path)
+    .filter(r => !!r);
+
+  res.status(200).json(routes);
 });
 
 module.exports = router;
