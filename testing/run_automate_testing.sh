0b7d1a383b15860bf74af0c925e9278efa24fb9e
Yong Hao TENG <yonghao.teng@gmail.com>
2018-03-04 01:31:16 -0800

    Change running instructions

--- testing/run_automate_testing.sh
+++ testing/run_automate_testing.sh
@@ -3,7 +3,7 @@
 # Check if taven is installed
 if ! hash tavern-ci 2>/dev/null; then
     echo -e 'tavern is not installed.'
-    echo -e 'Please install by it by typing pip install tavern'
+    echo -e 'Please install by it by typing sudo pip install tavern'
     exit 0
 else
     USER=`echo $RANDOM | md5sum | awk '{ print $1 }'`
